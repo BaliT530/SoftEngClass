e2d9086f7fd77691d15a623ef72d1ae22c8095f2
BaliT530 <true54321@gmail.com>
2019-01-22 15:36:40 -0500

    First Project Test

---
+++ src/hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class hello 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Please enter your name: ");
+		String s = reader.next();
+		System.out.println("User: " + s);
+	}
+
+}
