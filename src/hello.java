f215e25f1464285096f8e35f91b88dd446c7128d
Evan Solosky <soloskye538@strose.edu>
2019-01-22 16:30:14 -0500

    Updated by Evan

--- src/hello.java
+++ src/hello.java
@@ -9,6 +9,7 @@ public class hello
 		System.out.print("Please enter your name: ");
 		String s = reader.next();
 		System.out.println("User: " + s);
+		System.out.println("Updated by Evan");
 	}
 
 }
